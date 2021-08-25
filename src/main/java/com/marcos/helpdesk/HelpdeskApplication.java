f3d5961e7601bc67cbc9dcbf15343e16aee95b09
Marcos Ruiz Freire <marcosruizmrf@outlook.com>
2021-08-24 22:18:01 -0300

    Development profile

--- src/main/java/com/marcos/helpdesk/HelpdeskApplication.java
+++ src/main/java/com/marcos/helpdesk/HelpdeskApplication.java
@@ -1,48 +1,13 @@
 package com.marcos.helpdesk;
 
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.marcos.helpdesk.domain.Chamado;
-import com.marcos.helpdesk.domain.Cliente;
-import com.marcos.helpdesk.domain.Tecnico;
-import com.marcos.helpdesk.domain.enums.Perfil;
-import com.marcos.helpdesk.domain.enums.Prioridade;
-import com.marcos.helpdesk.domain.enums.Status;
-import com.marcos.helpdesk.repositories.ChamadoRepository;
-import com.marcos.helpdesk.repositories.ClienteRepository;
-import com.marcos.helpdesk.repositories.TecnicoRepository;
-
 @SpringBootApplication
-public class HelpdeskApplication implements CommandLineRunner {
-	
-	@Autowired
-	private TecnicoRepository tecnicoRepository;
-	@Autowired
-	private ClienteRepository clienteRepository;
-	@Autowired
-	private ChamadoRepository chamadoRepository;
-	
+public class HelpdeskApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(HelpdeskApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		Tecnico tec1 = new Tecnico(null, "Marcos Ruiz Freire", "12312312343", "marcos@mail.com", "123");
-		tec1.addPerfil(Perfil.ADMIN);
-		
-		Cliente cli1 = new Cliente(null, "Linus Torvalds", "32132132132", "torvalds@mail.com", "321");
-		
-		Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 01", "Primeiro chamado", tec1, cli1);
-		
-		tecnicoRepository.saveAll(Arrays.asList(tec1));
-		clienteRepository.saveAll(Arrays.asList(cli1));
-		chamadoRepository.saveAll(Arrays.asList(c1));
 	}
 
 }
