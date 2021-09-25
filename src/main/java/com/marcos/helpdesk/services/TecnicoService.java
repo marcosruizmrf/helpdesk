a891ab471ab0e5072212192522b305072a0831bd
Marcos Ruiz Freire <marcosruizmrf@outlook.com>
2021-09-25 00:56:11 -0300

    Exception to not found object

--- src/main/java/com/marcos/helpdesk/services/TecnicoService.java
+++ src/main/java/com/marcos/helpdesk/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.marcos.helpdesk.domain.Tecnico;
 import com.marcos.helpdesk.repositories.TecnicoRepository;
+import com.marcos.helpdesk.services.exceptions.ObjectnotFoundException;
 
 @Service
 public class TecnicoService {
@@ -17,7 +18,7 @@ public class TecnicoService {
 	
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
-		return obj.orElse(null);		
+		return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado! Id: " + id));		
 	}
 
 }
