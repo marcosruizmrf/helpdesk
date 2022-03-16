286910dc3473effc4dbe34083c97ddcdc30ee150
Marcos Ruiz Freire <marcosruizmrf@outlook.com>
2022-03-16 02:18:20 -0300

    feat: create to chamado

--- src/main/java/com/marcos/helpdesk/domain/dtos/ChamadoDTO.java
+++ src/main/java/com/marcos/helpdesk/domain/dtos/ChamadoDTO.java
@@ -25,11 +25,17 @@ public class ChamadoDTO {
 	private LocalDate dataAbertura = LocalDate.now();
 	@JsonFormat(pattern = "dd/MM/yyyy")
 	private LocalDate dataFechamento;
+	@NotNull(message = "O campo PRIORIDADE é requerido")
 	private Integer prioridade;
+	@NotNull(message = "O campo STATUS é requerido")
 	private Integer status;
+	@NotNull(message = "O campo TÍTULO é requerido")
 	private String titulo;
+	@NotNull(message = "O campo OBSERVAÇÔES é requerido")
 	private String observacoes;
+	@NotNull(message = "O campo TÉCNICO é requerido")
 	private Integer tecnico;
+	@NotNull(message = "O campo CLIENTE é requerido")
 	private Integer cliente;
 	private String nomeTecnico;
 	private String nomeCliente;
--- src/main/java/com/marcos/helpdesk/services/ChamadoService.java
+++ src/main/java/com/marcos/helpdesk/services/ChamadoService.java
@@ -3,10 +3,17 @@ package com.marcos.helpdesk.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.marcos.helpdesk.domain.Chamado;
+import com.marcos.helpdesk.domain.Cliente;
+import com.marcos.helpdesk.domain.Tecnico;
+import com.marcos.helpdesk.domain.dtos.ChamadoDTO;
+import com.marcos.helpdesk.domain.enums.Prioridade;
+import com.marcos.helpdesk.domain.enums.Status;
 import com.marcos.helpdesk.repositories.ChamadoRepository;
 import com.marcos.helpdesk.services.exceptions.ObjectnotFoundException;
 
@@ -15,6 +22,10 @@ public class ChamadoService {
 	
 	@Autowired
 	private ChamadoRepository repository;
+	@Autowired
+	private TecnicoService tecnicoService;
+	@Autowired
+	private ClienteService clienteService;
 	
 	public Chamado findById(Integer id) {
 		Optional<Chamado> obj = repository.findById(id);
@@ -23,6 +34,28 @@ public class ChamadoService {
 
 	public List<Chamado> findAll() {
 		return repository.findAll();
+	}
+
+	public Chamado create(@Valid ChamadoDTO objDTO) {
+		return repository.save(newChamado(objDTO));
+	}
+	
+	private Chamado newChamado(ChamadoDTO obj) {
+		Tecnico tecnico = tecnicoService.findById(obj.getTecnico());
+		Cliente cliente = clienteService.findById(obj.getCliente());
+		
+		Chamado chamado = new Chamado();
+		if(obj.getId() != null) {
+			chamado.setId(obj.getId());
+		}
+		
+		chamado.setTecnico(tecnico);
+		chamado.setCliente(cliente);
+		chamado.setPrioridade(Prioridade.toEnum(obj.getPrioridade()));
+		chamado.setStatus(Status.toEnum(obj.getStatus()));
+		chamado.setTitulo(obj.getTitulo());
+		chamado.setObservacoes(obj.getObservacoes());
+		return chamado;
 	}
 
 }
